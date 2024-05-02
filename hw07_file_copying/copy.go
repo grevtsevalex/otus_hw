ca2c965019cf5fc04663789a51858a26d2f3c172
grevtsevalex <aleksandr.grevtsev97@gmail.com>
2024-05-02 21:40:35 +1000

    hw07 is complete

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -10,6 +14,70 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	fi, err := os.Stat(fromPath)
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+
+	if !fi.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+
+	size := fi.Size()
+
+	if offset > size {
+		return ErrOffsetExceedsFileSize
+	}
+
+	if limit > size {
+		limit = 0
+	}
+
+	source, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer source.Close()
+
+	destination, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer destination.Close()
+
+	var bytesToCopy int64
+	bytesToCopy = limit
+	if limit == 0 {
+		bytesToCopy = size - offset
+	}
+
+	if limit > (size - offset) {
+		bytesToCopy = size - offset
+	}
+
+	bar := pb.StartNew(int(bytesToCopy))
+
+	buf := make([]byte, bytesToCopy)
+
+	var bufOffset int64
+	for bufOffset < bytesToCopy {
+		read, err := source.ReadAt(buf[bufOffset:], offset)
+		bufOffset += int64(read)
+		bar.Add64(int64(read))
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+	}
+
+	_, err = destination.Write(buf)
+	if err != nil {
+		return err
+	}
+
+	bar.Finish()
+
 	return nil
 }
