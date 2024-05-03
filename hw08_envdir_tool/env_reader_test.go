def97a8d33c9c3995a121ec20f55adba2df2d494
Grevcev.AS <grevcev.as@dns-shop.ru>
2024-05-03 15:27:29 +1000

    HW8 is completed

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,28 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	t.Run("simple case", func(t *testing.T) {
+		envs, err := ReadDir("./testdata/env")
+		require.NoError(t, err)
+
+		require.Equal(t, 5, len(envs))
+		require.True(t, envs["UNSET"].NeedRemove)
+
+		require.Equal(t, "bar", envs["BAR"].Value)
+		require.Equal(t, `   foo
+with new line`, envs["FOO"].Value)
+		require.Equal(t, "", envs["EMPTY"].Value)
+		require.Equal(t, "\"hello\"", envs["HELLO"].Value)
+	})
+
+	t.Run("bad dir", func(t *testing.T) {
+		_, err := ReadDir("./testdata/123123")
+		require.ErrorIs(t, err, ErrBadDir)
+	})
 }
--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,27 @@
 package main
 
+import (
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	for envName, e := range env {
+		if e.NeedRemove {
+			os.Unsetenv(envName)
+			continue
+		}
+
+		os.Setenv(envName, e.Value)
+	}
+
+	command := exec.Command(cmd[0], cmd[1:]...)
+	command.Stdout = os.Stdout
+	command.Stdin = os.Stdin
+	command.Stderr = os.Stderr
+
+	command.Run()
+
+	return command.ProcessState.ExitCode()
 }
