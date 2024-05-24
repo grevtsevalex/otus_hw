ce2bead6a843409ef034a6dbca4ba7b62f77105e
Grevcev.AS <grevcev.as@dns-shop.ru>
2024-05-24 11:33:57 +1000

    HW11 is completed

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,77 @@
 package main
 
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"time"
+)
+
+var (
+	timeout   time.Duration
+	ErrNoPort = errors.New("you must write a port")
+	ErrNoHost = errors.New("you must write host")
+)
+
+func init() {
+	flag.DurationVar(&timeout, "timeout", time.Second*10, "timeout for connection")
+}
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	flag.Parse()
+	switch len(flag.Args()) {
+	case 0:
+		fmt.Println(ErrNoHost)
+		return
+	case 1:
+		fmt.Println(ErrNoPort)
+		return
+	default:
+	}
+
+	host := flag.Arg(0)
+	port := flag.Arg(1)
+	address := net.JoinHostPort(host, port)
+	in := os.Stdin
+	out := os.Stdout
+
+	client := NewTelnetClient(address, timeout, in, out)
+
+	err := client.Connect()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "connect error: %v", err)
+		return
+	}
+	defer client.Close()
+
+	interruptCh := make(chan os.Signal, 1)
+
+	go func() {
+		err := client.Send()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "send error: %v", err)
+			interruptCh <- os.Interrupt
+		}
+
+		os.Stderr.WriteString("\nEOF...")
+		interruptCh <- os.Interrupt
+	}()
+
+	go func() {
+		err := client.Receive()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error from server: %v", err)
+			interruptCh <- os.Interrupt
+		}
+
+		os.Stderr.WriteString("\nConnection was closed by peer...")
+		interruptCh <- os.Interrupt
+	}()
+
+	signal.Notify(interruptCh, os.Interrupt)
+	<-interruptCh
+	os.Stderr.WriteString("\nBye-bye, client!")
 }
